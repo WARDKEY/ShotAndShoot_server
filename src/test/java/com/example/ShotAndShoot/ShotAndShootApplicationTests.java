208805b128a467c6ad00f304ca7d224342195ed1
WARDKEY <tjrmsduq@naver.com>
2024-11-20 15:38:11 +0900

    server 초기세팅

---
+++ src/test/java/com/example/ShotAndShoot/ShotAndShootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ShotAndShoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShotAndShootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
