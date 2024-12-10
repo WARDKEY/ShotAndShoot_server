b49a202f996e368aada08671e2b60ee4c0b649cc
tgy1201 <dndsm82@kyonggi.ac.kr>
2024-12-10 17:20:19 +0900

    feat(wasteCompany):폐기물업체 기능 추가

---
+++ src/main/java/com/example/ShotAndShoot/domain/wasteCompany/controller/WasteCompanyController.java
@@ -0,0 +1,30 @@
+package com.example.ShotAndShoot.domain.wasteCompany.controller;
+
+import com.example.ShotAndShoot.domain.wasteCompany.dto.WasteCompanyResponseDTO;
+import com.example.ShotAndShoot.domain.wasteCompany.service.WasteCompanyService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Slf4j
+@RequestMapping("/api/v1/wasteCompany")
+@RestController
+@RequiredArgsConstructor
+public class WasteCompanyController {
+
+    private final WasteCompanyService wasteCompanyService;
+
+    /**
+     * 폐기물업체 조회
+     */
+    @GetMapping("/")
+    public ResponseEntity<List<WasteCompanyResponseDTO>> getAllWasteCompany() {
+        List<WasteCompanyResponseDTO> wasteCompanies = wasteCompanyService.getAllWasteCompany();
+        return ResponseEntity.ok(wasteCompanies);
+    }
+}
---
+++ src/main/java/com/example/ShotAndShoot/domain/wasteCompany/repository/WasteCompanyRepository.java
@@ -0,0 +1,8 @@
+package com.example.ShotAndShoot.domain.wasteCompany.repository;
+
+import com.example.ShotAndShoot.global.entity.WasteCompany;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WasteCompanyRepository extends JpaRepository<WasteCompany, Long> {
+
+}
