7697f38c13a5bdfc7b6eea4e2147e5f8679ce6af
tgy1201 <dndsm82@kyonggi.ac.kr>
2024-12-10 17:30:51 +0900

    fix(comment):댓글 기능 수정

--- src/main/java/com/example/ShotAndShoot/domain/comment/controller/CommentController.java
+++ src/main/java/com/example/ShotAndShoot/domain/comment/controller/CommentController.java
@@ -20,7 +20,6 @@ import java.util.List;
 public class CommentController {
 
     private final CommentService commentService;
-    private final CommentRepository commentRepository;
 
     @PostMapping("/{questionId}")
     public ResponseEntity<ResultMessageDTO> saveComment(@PathVariable("questionId") Long questionId,
