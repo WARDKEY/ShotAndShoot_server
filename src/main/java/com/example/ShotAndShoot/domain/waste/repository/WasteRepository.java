fcddc7f690e0ce6c884cf3bd7c87fff4c0301dd4
tgy1201 <dndsm82@kyonggi.ac.kr>
2024-12-17 15:27:18 +0900

    feat(waste):폐기물 이름 검색 기능 추가

--- src/main/java/com/example/ShotAndShoot/domain/waste/repository/WasteRepository.java
+++ src/main/java/com/example/ShotAndShoot/domain/waste/repository/WasteRepository.java
@@ -3,7 +3,9 @@ package com.example.ShotAndShoot.domain.waste.repository;
 import com.example.ShotAndShoot.global.entity.Waste;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface WasteRepository extends JpaRepository<Waste, Long> {
+import java.util.Optional;
 
+public interface WasteRepository extends JpaRepository<Waste, Long> {
+    Optional<Waste> findByWasteName(String wasteName);
 
 }
